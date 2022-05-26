64e41b75a6347a97201f0e1163ac3b3c0838001f
daGodz <uribae@protonmail.com>
2022-05-26 14:29:32 -0400

    d

--- front/pages/index.js
+++ front/pages/index.js
@@ -4,6 +4,8 @@ export default function Home() {
     <div>
         <h1 className="text-3xl font-bold underline">
       Hello world!fjfj
+
+      <a className="ugh">this is cool</a>
     </h1>
     </div>
   )
